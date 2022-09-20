6e69d13681248563454d90a04182872a692f8f7d
Mochamad Ramdhanie Mubarak <dhaniemubarak@gmail.com>
2022-09-20 14:24:46 +0700

    Account balance v1

--- src/main/java/id/holigo/services/holigoaccountbalanceservice/web/controllers/AccountBalanceController.java
+++ src/main/java/id/holigo/services/holigoaccountbalanceservice/web/controllers/AccountBalanceController.java
@@ -1,22 +1,18 @@
 package id.holigo.services.holigoaccountbalanceservice.web.controllers;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import id.holigo.services.common.model.AccountBalanceDto;
-import id.holigo.services.holigoaccountbalanceservice.config.JmsConfig;
 import id.holigo.services.holigoaccountbalanceservice.domain.AccountBalance;
 import id.holigo.services.holigoaccountbalanceservice.repositories.AccountBalanceRepository;
 import id.holigo.services.holigoaccountbalanceservice.web.mappers.AccountBalanceMapper;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.jms.core.JmsTemplate;
 import org.springframework.web.bind.annotation.*;
 
-import javax.jms.JMSException;
-import javax.jms.Message;
 import java.util.Optional;
 
+@Slf4j
 @RestController
 public class AccountBalanceController {
     private AccountBalanceRepository accountBalanceRepository;
@@ -34,9 +30,13 @@ public class AccountBalanceController {
 
     @GetMapping("/api/v1/accountBalance")
     public ResponseEntity<AccountBalanceDto> createAccountBalance(@RequestHeader("user-id") Long userId) {
+        log.info("userid -> {}", userId);
         Optional<AccountBalance> fetchAccountBalance = accountBalanceRepository.findById(userId);
-        return fetchAccountBalance.map(accountBalance -> new ResponseEntity<>(
-                        accountBalanceMapper.accountBalanceToAccountBalanceDto(accountBalance), HttpStatus.OK))
-                .orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
+        if (fetchAccountBalance.isPresent()) {
+            log.info("user id found");
+            return new ResponseEntity<>(accountBalanceMapper.accountBalanceToAccountBalanceDto(fetchAccountBalance.get()), HttpStatus.OK);
+        }
+        log.info("user id not found");
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
 }
